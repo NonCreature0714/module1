951a922bcd936453b558b2b2c059acc3ef9a74f9
Bill <brubaker.bill@gmail.com>
2016-09-17 18:36:04 -0500

    initial commit
    Personal “business” website created for class.

---
+++ prices.hourly.js
@@ -0,0 +1,6 @@
+//per Hour
+var iOSPerHour = 80;
+var androidDevPerHour = 100;
+var webDevPerHour = 60;
+var microSystemsPerHour = 120;
+var databasePerHour = 110;
---
+++ prices.project.js
@@ -0,0 +1,5 @@
+//per Project
+var makeMeAnApp = 60000;
+var consultOnMyApp = 20000;
+var designMyAssets = 60000;
+var fullPackage 140000;
